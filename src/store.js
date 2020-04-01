f7805531feec4cf9a6500e56a8401fd675e15916
Nick Farni <nickfarni001@gmail.com>
2020-04-01 17:27:40 -0600

    added CD routes

---
+++ src/store.js
@@ -0,0 +1,31 @@
+const bookmarks = [{
+    id: '1',
+    title: 'Google',
+    url: 'http://www.google.com',
+    description: 'World\'s most popular earch engine',
+    rating: '4'
+
+},
+{
+    id: '2',
+    title: 'SITE 2',
+    url: 'http://www.google.com',
+    description: 'World\'s most popular earch engine',
+    rating: '4'
+},
+{
+    id: '3',
+    title: 'SITE 3',
+    url: 'http://www.google.com',
+    description: 'World\'s most popular earch engine',
+    rating: '4'
+},
+{
+    id: '4',
+    title: 'SITE 4',
+    url: 'http://www.google.com',
+    description: 'World\'s most popular earch engine',
+    rating: '4'
+}]
+
+module.exports = { bookmarks };
